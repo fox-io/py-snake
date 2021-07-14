839c5d8a5c54b3a484dbc954cfa65815388fada3
fox-io <gitlab.fox-io@foxdata.io>
2021-07-14 06:48:43 -0500

    Add implementation for food. Random spawning.

--- food.py
+++ food.py
@@ -1,7 +1,21 @@
 from turtle import Turtle
+from random import randint
 
 
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        pass
+        self.penup()
+        self.shape("turtle")
+        self.color("green")
+
+        self.respawn()
+
+    def respawn(self):
+        """Generating random coordinates for the food location.
+
+        * 280 is the min/max playing area in which the turtle will be fully visible.
+        """
+        pos_x = randint(-280, 280)
+        pos_y = randint(-280, 280)
+        self.goto(pos_x, pos_y)
