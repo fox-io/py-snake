b66ae16a67c69f9f737d3e0796b465322cd852d7
fox-io <gitlab.fox-io@foxdata.io>
2021-07-14 06:07:27 -0500

    Setup game window.

--- main.py
+++ main.py
@@ -5,10 +5,28 @@ Snake Game
 
 (c)2021 John Mann <gitlab.fox-io@foxdata.io>
 """
+from turtle import Screen
+from time import sleep
 
 
 def main():
-    pass
+    # Setup game window
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("py-snake")
+
+    # Disable auto-refreshing animation
+    screen.tracer(0)
+
+    # Listen for keybinds
+    screen.listen()
+
+    # Main game loop
+    running = True
+    while running:
+        sleep(0.1)
+        screen.update()
 
 
 if __name__ == "__main__":
