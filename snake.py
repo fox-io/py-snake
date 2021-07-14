c6571816953a9b5e5a28635d9229a0171172d6a3
fox-io <gitlab.fox-io@foxdata.io>
2021-07-14 06:19:17 -0500

    Add body segments for snake.

--- snake.py
+++ snake.py
@@ -1,7 +1,18 @@
 from turtle import Turtle
 
 
-class Snake(Turtle):
+class Snake:
     def __init__(self):
-        super().__init__()
-        pass
+        self.body = []
+        self.speed = 0.1
+        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+        for starting_position in self.starting_positions:
+            self.add_segment(starting_position)
+
+    def add_segment(self, position):
+        t = Turtle("square")
+        t.penup()
+        t.color("white")
+        t.setpos(position)
+        self.body.append(t)
