00361baa2adb2b2bd6fac2151c609ea02e8e83fe
fox-io <gitlab.fox-io@foxdata.io>
2021-07-14 06:51:24 -0500

    Add score system. Added game over state.

--- score.py
+++ score.py
@@ -4,4 +4,20 @@ from turtle import Turtle
 class Score(Turtle):
     def __init__(self):
         super().__init__()
-        pass
+        self.hideturtle()
+        self.color("white")
+        self.goto(0, 280)
+        self.score = 0
+        self.update_text()
+
+    def add_point(self):
+        self.score += 1
+        self.update_text()
+
+    def update_text(self):
+        self.clear()
+        self.write(f"Score: {self.score}", False, align="center", font=("Arial", 14, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", False, align="center", font=("Arial", 14, "normal"))
